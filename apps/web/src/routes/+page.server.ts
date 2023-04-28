d33e099d8799b1c0fec60d9d1201a0a2b4350f52
Sam Peterson <samllama55@gmail.com>
2023-04-28 22:58:27 +0200

    Fix deploy.

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,3 +1,4 @@
+import { error } from '@sveltejs/kit';
 import { ZodRoomMapServer, type RoomMapServer } from 'planning-poker-types';
 
 export const load = async ({ fetch }) => {
@@ -5,6 +6,7 @@ export const load = async ({ fetch }) => {
 	const data = await res.json();
 
 	const rooms: RoomMapServer[] = [];
+	if (!Array.isArray(data)) throw error(500, { message: 'Invalid room data' });
 	data.forEach((room: unknown) => {
 		const parsedRoom = ZodRoomMapServer.parse(room);
 		rooms.push(parsedRoom);
