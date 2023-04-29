96eef9f5de6c729dc8b359c40c5b63a6f88904de
Sam Peterson <samllama55@gmail.com>
2023-04-29 17:47:39 +0200

    ENV Var for prod api url.

--- apps/web/src/lib/apiUrl.ts
+++ apps/web/src/lib/apiUrl.ts
@@ -1,4 +1,3 @@
 export const API_URL = import.meta.env.DEV
 	? `http://localhost:5173/api`
-	: 'https://planning-poker-turbo-sveltekit-api.onrender.com/api';
-// : `${import.meta.env.VITE_API_URL}`;
+	: `${import.meta.env.VITE_FULL_API_URL}`;
