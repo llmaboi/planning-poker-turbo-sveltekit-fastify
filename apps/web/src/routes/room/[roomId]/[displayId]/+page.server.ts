42b1f6eedd71e1a6aa4d9c8c0691b0577b66d9e7
Sam <samllama55@gmail.com>
2023-07-22 15:43:53 +0200

    CHANGE: room->display view to use  forms instead of js where possible. REMOVE: Un-necessary variables.

--- apps/web/src/routes/room/[roomId]/[displayId]/+page.server.ts
+++ apps/web/src/routes/room/[roomId]/[displayId]/+page.server.ts
@@ -1,7 +1,8 @@
+import { API_URL } from '$lib/apiUrl';
+import { redirect } from '@sveltejs/kit';
 import { ZodRoomMapServer } from 'planning-poker-types';
-import { API_URL } from '../../../../lib/apiUrl';
 
-export const load = async ({ fetch, params }) => {
+export const load = async ({ fetch, params, url }) => {
 	const res = await fetch(`${API_URL}/rooms/${params.roomId}`);
 	const data = await res.json();
 
@@ -16,4 +17,48 @@ export const load = async ({ fetch, params }) => {
 		room,
 		currentDisplay
 	};
+};
+
+export const actions = {
+	select: async ({ request, params }) => {
+		const data = await request.formData();
+		const rawSelected = data.get('selected');
+		const maybeNumber = Number(rawSelected);
+		let selected = 0;
+
+		if (isNaN(maybeNumber)) {
+			selected = 0;
+		} else {
+			selected = maybeNumber;
+		}
+
+		await fetch(`${API_URL}/displays/${params.displayId}`, {
+			method: 'PUT',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				roomId: params.roomId,
+				cardValue: selected
+			})
+		});
+
+		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
+	},
+	reset: async ({ params }) => {
+		await fetch(`${API_URL}/displays/${params.displayId}`, {
+			method: 'PUT',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				roomId: params.roomId,
+				cardValue: 0
+			})
+		});
+
+		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
+	}
 };
