d41b18102ce0126400449519ae8bb2566be316ed
Sam <samllama55@gmail.com>
2023-08-03 01:17:19 +0200

    CHANGE: Formify the admin label and reset.

--- apps/web/src/routes/room/[roomId]/[displayId]/+page.server.ts
+++ apps/web/src/routes/room/[roomId]/[displayId]/+page.server.ts
@@ -1,6 +1,6 @@
 import { API_URL } from '$lib/apiUrl';
 import { redirect } from '@sveltejs/kit';
-import { ZodRoomMapServer } from 'planning-poker-types';
+import { ZodRoomMapServer, type Room } from 'planning-poker-types';
 
 export const load = async ({ fetch, params, url }) => {
 	const res = await fetch(`${API_URL}/rooms/${params.roomId}`);
@@ -57,6 +57,58 @@ export const actions = {
 				roomId: params.roomId,
 				cardValue: 0
 			})
+		});
+
+		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
+	},
+	resetAll: async ({ params }) => {
+		await fetch(`${API_URL}/rooms/${params.roomId}/card-reset`, {
+			method: 'PATCH',
+			headers: {
+				Accept: 'application/json'
+			}
+		});
+
+		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
+	},
+	toggleVotes: async ({ params }) => {
+		const res = await fetch(`${API_URL}/rooms/${params.roomId}`);
+		const roomData = await res.json();
+		const oldRoom = ZodRoomMapServer.parse(roomData);
+
+		await fetch(`${API_URL}/rooms/${params.roomId}`, {
+			method: 'PATCH',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				...oldRoom,
+				showVotes: !oldRoom.showVotes
+			})
+		});
+
+		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
+	},
+	label: async ({ request, params }) => {
+		const data = await request.formData();
+		const roomLabel = String(data.get('roomLabel')) ?? '';
+		const res = await fetch(`${API_URL}/rooms/${params.roomId}`);
+		const roomData = await res.json();
+		const oldRoom = ZodRoomMapServer.parse(roomData);
+
+		const room: Room = {
+			...oldRoom,
+			label: roomLabel
+		};
+
+		await fetch(`${API_URL}/rooms/${params.roomId}`, {
+			method: 'PATCH',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(room)
 		});
 
 		throw redirect(303, `/room/${params.roomId}/${params.displayId}`);
